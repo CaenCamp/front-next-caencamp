f1b51926302473d149362f7ddaf3251e4756b5eb
Alexis Janvier <contact@alexisjanvier.net>
2021-08-28 14:36:52 +0200

    Site skeleton WIP

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+export const Footer = ({ styles }) => (
+    <footer className={styles.footer}>
+        <p>Le footer</p>
+    </footer>
+);
---
+++ pages/evenements/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+import styles from '../../styles/Home.module.css';
+import { Favicon } from '../../components/Favicon';
+import { Footer } from '../../components/Footer';
+import { Menu } from '../../components/Menu';
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Les événements CaenCamp</title>
+                <Favicon />
+            </Head>
+
+            <main className={styles.main}>
+                <Menu styles={styles} />
+                <h1 className={styles.title}>Les événements</h1>
+
+                <p className={styles.description}>Liste avec filtre par tags</p>
+                <ul>
+                    <li>
+                        <Link href="/evenements/elixir-pourquoi-pas">Elixir ? Pourquoi pas.</Link>
+                    </li>
+                </ul>
+            </main>
+
+            <Footer styles={styles} />
+        </div>
+    );
+}
