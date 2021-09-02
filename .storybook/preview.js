92ac57db72fab93583298930bbac3a51deac26d9
Alexis Janvier <contact@alexisjanvier.net>
2021-09-02 10:34:01 +0200

    Bootstrap storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+
+export const parameters = {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+    nextRouter: {
+        Provider: RouterContext.Provider,
+        path: '/', // defaults to `/`
+        asPath: '/', // defaults to `/`
+        query: {}, // defaults to `{}`
+        push() {}, // defaults to using addon actions integration,
+        //   can override any method in the router
+    },
+};
