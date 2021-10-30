7256713fe86694052e5a18ca282e9dda82f7e1eb
Alexis Janvier <contact@alexisjanvier.net>
2021-10-30 09:15:09 +0200

    feat(seo): ajout du jsonLd CaenCamp en home

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,30 @@ import illustration from '../../public/illustrations/undraw_open_source_1qxw.svg
 
 const API_URL = process.env.API_URL;
 
+const caenCampJsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    address: {
+        '@type': 'PostalAddress',
+        addressLocality: 'Caen, France',
+        addressCountry: 'FR',
+        postalCode: '14000',
+    },
+    email: 'contact(at)caen.camp',
+    name: 'CaenCamp',
+    disambiguatingDescription:
+        'Le CaenCamp est un collectif ouvert à tous.tes les développeur.euses de Caen et du Calvados, que ces personnes soient juniors, séniores, professionnelles, étudiantes ou juste curieuses.',
+    logo: {
+        '@type': 'ImageObject',
+        author: 'CaenCamp',
+        contentLocation: 'Caen, Calvados, France',
+        contentUrl: 'https://caen.camp/static/logoFondBlanc-278da657a83902f7d21083ade8e9ce7a.png',
+        datePublished: '2012-09-09',
+        description: 'Le logo du CaenCamp.',
+        name: 'Le logo du CaenCamp',
+    },
+};
+
 export async function getStaticProps() {
     const events = await fetch(`${API_URL}/events?category=CaenCamp&perPage=4`)
         .then(apiResponse => apiResponse.json())
@@ -33,7 +57,12 @@ export async function getStaticProps() {
     };
 }
 
-export default function Home({ events }) {
+export default function Home({
+    events = {
+        upcomming: [],
+        past: [],
+    },
+}) {
     return (
         <div className="home">
             <Head>
@@ -41,6 +70,15 @@ export default function Home({ events }) {
                 <meta
                     name="description"
                     content="Bienvenue sur le site du CaenCamp, collectif ouvert à tous.tes les développeur.euses du Calvados"
+                />
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify({
+                            ...caenCampJsonLd,
+                            events: [...events.upcomming, ...events.past.slice(0, 2)],
+                        }),
+                    }}
                 />
             </Head>
             <div className="u-full-width hero">
