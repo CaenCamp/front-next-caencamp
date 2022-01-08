9d918f13968e32ae98f251c64b743ef8b1aa48cb
Alexis Janvier <contact@alexisjanvier.net>
2022-01-08 12:37:14 +0100

    feat: submit proposal to the real api

--- src/pages/api/call-for-speaker.js
+++ src/pages/api/call-for-speaker.js
@@ -1,8 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios';
 
-export default function handler(req, res) {
+const submitPaper = async paper => {
+    const submitedPaperResponse = await axios({
+        url: `/papers`,
+        method: 'post',
+        baseURL: process.env.API_URL,
+        headers: {
+            authorization: `Bearer ${process.env.API_TOKEN}`,
+            'Content-Type': 'application/json',
+        },
+        data: paper,
+    });
+
+    if (submitedPaperResponse.status !== 200) {
+        throw new Error(
+            `Problème lors de la soumission de votre proposition avec un code ${submitedPaperResponse.status}. Vous pouvez contacter les administrateurs du site (contact@coding.caen.camp).`,
+        );
+    }
+
+    return submitedPaperResponse.data;
+};
+
+export default async function handler(req, res) {
     if (req.method === 'POST') {
-        res.status(200).json(req.body);
+        try {
+            const submitedPaper = await submitPaper(req.body);
+            res.status(200).json(submitedPaper);
+        } catch (error) {
+            res.status(400).json({ message: error.message });
+        }
     } else {
         res.status(404);
     }
