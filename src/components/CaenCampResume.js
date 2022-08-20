7ae3697919f42f03f406fef3dd9bca6240ee502d
Alexis Janvier <contact@alexisjanvier.net>
2022-08-20 08:54:21 +0200

    chore: fix pour eslint et donc le build

--- src/components/CaenCampResume.js
+++ src/components/CaenCampResume.js
@@ -5,8 +5,8 @@ const Resume = () => (
             Calvados, que ces personnes soient juniors, séniores, professionnelles, étudiantes ou juste curieuses.
         </p>
         <p>
-            Une rencontre, gratuite, est organisée régulièrement autour de sujets
-            présentés par des membres du collectif souhaitant partager leur expérience.
+            Une rencontre, gratuite, est organisée régulièrement autour de sujets présentés par des membres du collectif
+            souhaitant partager leur expérience.
         </p>
         <p>
             Ces sujets peuvent concerner n'importe quel langage de programmation, voir d'autres sujets que la
