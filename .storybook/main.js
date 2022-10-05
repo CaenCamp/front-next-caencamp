f49d5659db05781545582c235668eaa703fdafe2
Alexis Janvier <alexis.janvier@incaya.fr>
2022-10-05 07:51:59 +0200

    chore: suppression du Storybook
    le cout de la maintenance est superieur au gain apporte

--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    stories: ['../src/components/**/*.stories.js'],
-    addons: [
-        'storybook-addon-next-router',
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/addon-a11y',
-    ],
-};
